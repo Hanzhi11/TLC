1a883e855f17ab8dd7811ffef22ee4a2465d0a91
Hanzhi <hanzhizhang11@gmail.com>
2024-08-10 16:51:03 +1000

    completed the functionality of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,114 @@
-import bin from './assets/bin.svg'
-import lightning from './assets/lightning.svg'
-import './App.css'
-import Numbers from './components/Numbers'
+import bin from "./assets/bin.svg";
+import lightning from "./assets/lightning.svg";
+import "./App.css";
+import Numbers from "./components/Numbers";
+import { useState } from "react";
 
-function App() {
-  const index = 1
-  return (
-    <div>
-        <div>{index}</div>
-        <Numbers />
-        <div>
-          <button className='selectButton'>
-            <img src={lightning} className="select" alt="Select Icon" />
-  </button>
-          <button className='clearButton'><img src={bin} className="clear" alt="Clear Icon" />
-  </button>
-        </div>
-    </div>
-  )
+export interface PowerballNumbers {
+    primary: number[];
+    secondary: number;
 }
 
-export default App
+const initialSelectedNumbers: number[] = new Array(7).fill(0);
+const index: number = 1;
+
+const payload = {
+    CompanyId: "GoldenCasket",
+    MaxDrawCountPerProduct: 1,
+    OptionalProductFilter: ["Powerball"],
+};
+
+const initialNumbers = {
+    primary: initialSelectedNumbers,
+    secondary: 0,
+};
+
+function validNumbers(numbers: Set<number>): boolean {
+    let isValid = true;
+    for (const num of numbers) {
+        if (num < 1 || num > 35) {
+            isValid = false;
+            break;
+        }
+    }
+    return isValid;
+}
+
+function App(): JSX.Element {
+    const [numbers, setNumbers] = useState<PowerballNumbers>(initialNumbers);
+
+    const retriveNumbers = (): void => {
+        const currentPrimaryNumbers = new Set(numbers.primary);
+        if (currentPrimaryNumbers.size === 7) {
+            return;
+        }
+        fetch(
+            "https://data.api.thelott.com/sales/vmax/web/data/lotto/latestresults",
+            {
+                method: "Post",
+                body: JSON.stringify(payload),
+            }
+        )
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`Data fetch failed! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                const result = data.DrawResults[0];
+                if (data.Success && result?.ProductId === "Powerball") {
+                    const primaryNumbers: Set<number> = new Set(
+                        result.PrimaryNumbers
+                    );
+                    if (
+                        primaryNumbers.size !== 7 ||
+                        !validNumbers(primaryNumbers)
+                    ) {
+                        return;
+                    }
+                    setNumbers({
+                        primary: result.PrimaryNumbers,
+                        secondary: result.SecondaryNumbers[0],
+                    });
+                }
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    };
+
+    const clearNumbers = (): void => {
+        const currentPrimaryNumbers = new Set(numbers.primary);
+        if (
+            currentPrimaryNumbers.size === 1 &&
+            currentPrimaryNumbers.values().next().value === 0
+        ) {
+            return;
+        }
+        setNumbers(initialNumbers);
+    };
+
+    return (
+        <div className="appContainer">
+            <header className="titleContainer">
+                <span className="title">Game </span>
+                {index}
+            </header>
+            <Numbers numbers={numbers} />
+            <div className="buttonContainer">
+                <button
+                    className="selectButton circle"
+                    onClick={retriveNumbers}
+                >
+                    <img src={lightning} alt="Select Icon" />
+                </button>
+                <button className="clearButton circle" onClick={clearNumbers}>
+                    <img src={bin} alt="Clear Icon" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -1,5 +1,77 @@
-export default function Numbers () {
-    return <div>
-        <h1>Numbers</h1>
-    </div>
+import { PowerballNumbers } from "../App";
+import cross from "./../assets/cross.svg";
+
+interface Props {
+    numbers: PowerballNumbers;
+}
+
+function generateCells(
+    totalNumber: number,
+    selectedNumbers: number[]
+): JSX.Element {
+    return (
+        <section className="grid">
+            {new Array(totalNumber).fill(null).map((_element, index) => {
+                let className = "cell";
+                if (selectedNumbers.includes(index + 1)) {
+                    className += " " + "cellSelected";
+                }
+                return (
+                    <div key={index} className={className}>
+                        <div
+                            hidden={!selectedNumbers.includes(index + 1)}
+                            className="crossContainer"
+                        >
+                            <img src={cross} alt="Cross Icon" />
+                        </div>
+                        <b style={{ color: "#9a4edd", zIndex: "0" }}>
+                            {index + 1}
+                        </b>
+                    </div>
+                );
+            })}
+        </section>
+    );
+}
+
+export default function Numbers(props: Props): JSX.Element {
+    const { primary, secondary } = props.numbers;
+    const set = new Set(primary);
+    const primaryCells = generateCells(35, primary);
+    const secondaryCells = generateCells(20, [secondary]);
+
+    return (
+        <div>
+            <section className="selectionContainer">
+                {primary.map((number, index) => {
+                    let className = "circle number";
+                    if (number !== 0) {
+                        className += " " + "primarySelected";
+                    } else if (set.size === index + 1) {
+                        className += " " + "selected";
+                    } else {
+                        className += " " + "notSelected";
+                    }
+
+                    return (
+                        <div key={`selected_${index}`} className={className}>
+                            <b>{number !== 0 && number}</b>
+                        </div>
+                    );
+                })}
+                <div
+                    className={"circle number".concat(
+                        secondary === 0 ? "" : " secondarySelected"
+                    )}
+                >
+                    <b>{secondary ? secondary : "PB"}</b>
+                </div>
+            </section>
+            {primaryCells}
+            <p className="runner">
+                <b>SELECT YOUR POWERBALL</b>
+            </p>
+            {secondaryCells}
+        </div>
+    );
 }
